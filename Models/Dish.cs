37e80b6b20ff13d8279750f62a47589183c33f05
NaamaLaufer <naama4212@gmail.com>
2026-06-21 11:57:33 +0300

    Added Models for Category, Dish, Order and OrderDish with relationships

---
+++ Models/Dish.cs
@@ -0,0 +1,14 @@
+﻿namespace restaurant_orders_api.Models
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public ICollection<OrderDish> OrderDishes { get; set; }
+    }
+}
