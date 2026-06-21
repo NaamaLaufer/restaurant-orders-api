d44b4dd1eda973fd25c705e1868e3fcf1998b5c2
NaamaLaufer <naama4212@gmail.com>
2026-06-21 17:59:46 +0300

    add centralized error handling middleware

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseMiddleware<restaurant_orders_api.Middleware.ExceptionHandlingMiddleware>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
